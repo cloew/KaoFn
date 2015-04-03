d47af6a5e4958a66df6248f2848e3b1fb756afcf
Chris Loew <cloew123@gmail.com>
2015-04-03 17:17:34 -0600

    Tidied up function metadata so it can handle functions with no defaults

--- src/kao_fn/function_metadata.py
+++ src/kao_fn/function_metadata.py
@@ -7,6 +7,9 @@ class FunctionMetadata:
     def __init__(self, func):
         """ Initialize the metadata """
         args, varargs, keywords, defaults = inspect.getargspec(func)
-        self.argToIndex = {arg:args.index(arg) for arg in args}
-        self.nameToDefaultValue = dict(zip(reversed(args), reversed(defaults)))
-        self.nameToArg = {arg:SmartArg(arg, self.argToIndex[arg]) for arg in args}
+        print(defaults)
+        self.argNameToIndex = {arg:args.index(arg) for arg in args}
+        self.nameToDefaultValue = dict(zip(reversed(args), reversed(defaults))) if defaults is not None else {}
+        
+        self.args = [SmartArg(arg, self.argNameToIndex[arg]) for arg in args]
+        self.nameToArg = {arg.argName:arg for arg in self.args}
